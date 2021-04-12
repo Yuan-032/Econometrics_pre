353119c2eba04568ce4172f21eddf5ecb3f41b5f
SheyaJi <82353332+SheyaJi@users.noreply.github.com>
2021-04-11 23:55:18 -0400

    Add files via upload
    change the line 14 to switch the year order so we use 8090 as reference year

--- regression.R
+++ regression.R
@@ -11,6 +11,6 @@ df$dummy1018 <- ifelse(df$Year >= 2010 , 1, 0)
 
 df <- dummy_cols(df, select_columns = "State")
 
-fm0 <- lm(unemployment_rate ~ Effective.Minimum.Wage.2020.Dollars + dummy8090 + dummy9000+dummy0010+dummy1018 , data = df)
+fm0 <- lm(unemployment_rate ~ Effective.Minimum.Wage.2020.Dollars + dummy9000+dummy0010+dummy1018+ dummy8090  , data = df)
 summary(fm0)
 coeftest(fm0, df = Inf, vcov = vcovHC(fm0, type = "HC1"))
